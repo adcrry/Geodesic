d67bbac80b8e4d4c6f1f537298becc6eedbdab66
Vincent Boulard <vboulard07@gmail.com>
2023-03-28 20:04:01 +0200

    first version

--- geodesics.py
+++ geodesics.py
@@ -71,16 +71,18 @@ def F_gamma(X,gamma):
                     s += X[i+2]*X[j+2]*gamma[k-2][j].subs([(x, X[0]), (y, X[1])]).evalf()
                 else:
                     s += X[i+2]*X[j+2]*gamma[k-2][j+1].subs([(x, X[0]), (y, X[1])]).evalf()
-        Y.append(simplify(-s))
+        Y.append(-s)
     return [X[2],X[3],Y[0],Y[1]]
 
 #Parameterization of the manifold as an embedded submanifold of R^3
-f = [x,y,x*y] #f : U -> R^3 with U an open subset of R^2
+#WARNING : Remember that a parametrization f need to be an immersion i.e. df never vanishes, if not it is possible that g^-1 diverge !
+f = [x,-y,x*y] #f : U -> R^3 with U an open subset of R^2
+f_l = [lambdify([x,y], f[0]), lambdify([x,y], f[1]), lambdify([x,y], f[2])] #Converting f to a "numerical" function and not a formal one
 
-D_f = 5 #[-D_f,D_f]^2 is a square domain center at 0 where f is defined
+D_f = 1 #[-D_f,D_f]^2 is a square domain center at 0 where f is defined
 X_0 = [0,0,0.5,-1] #initial condition as [x_1,x_2,dx_1/dt,dx_2/dt] where x_i is the ith coordinate of the curve defined by f^-1 on the manifold
 
-X_0 = normalize_speed(X_0) #normalise the initial velocity vector
+X_0 = normalize_speed(X_0) #normalization of the initial velocity vector
 
 g, inv_g = metric(f)
 gamma = christoffel(g, inv_g)
@@ -91,7 +93,7 @@ t_eval = np.arange(0, D_f, 0.1) #if x_1=x_2=0 be aware if not and in general to 
 sol = solve_ivp(F, [0, D_f], X_0, t_eval=t_eval)
 
 
-geodesic = np.array([lambdify([x,y], f[0])(sol.y[0],sol.y[1]),  lambdify([x,y], f[1])(sol.y[0],sol.y[1]), lambdify([x,y], f[2])(sol.y[0],sol.y[1])])
+geodesic = np.array([f_l[0](sol.y[0],sol.y[1]),  f_l[1](sol.y[0],sol.y[1]), f_l[2](sol.y[0],sol.y[1])])
 
 
 fig = plt.figure()
@@ -105,13 +107,13 @@ Z = geodesic[2]
 
 ax.plot(X, Y, Z, '-r', linewidth = 3)
 
-X = np.linspace(-D_f, D_f, 30)
-Y = np.linspace(-D_f, D_f, 30)
 
-X, Y = np.meshgrid(X,Y)
+U,V = np.meshgrid(np.linspace(-D_f, D_f, 30),np.linspace(-D_f, D_f, 30))
 
-Z = lambdify([x,y], f[2])(X,Y)
-#
+X = f_l[0](U,V)
+Y = f_l[1](U,V)
+Z = f_l[2](U,V)
+
 ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='viridis', edgecolor='none') #for now, the surface need to be defined like this f=[x,y,g(x,y)]
 
 ax.set_xlabel('x')
